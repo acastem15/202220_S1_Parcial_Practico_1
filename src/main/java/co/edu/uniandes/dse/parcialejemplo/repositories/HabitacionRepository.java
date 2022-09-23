ea6fa37ab753e85ed84d8ca593009e9927321bde
acastem15 <a.castellanosm@uniandes.edu.co>
2022-09-23 06:55:35 -0500

    entities y respositories

---
+++ src/main/java/co/edu/uniandes/dse/parcialejemplo/repositories/HabitacionRepository.java
@@ -0,0 +1,16 @@
+package co.edu.uniandes.dse.parcialejemplo.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.uniandes.dse.parcialejemplo.entities.HabitacionEntity;
+
+@Repository
+public interface HabitacionRepository extends JpaRepository<HabitacionEntity, Long> {
+
+    Optional<HabitacionEntity> findById(Long id);
+    Optional<HabitacionEntity> findByNombre(String nombre); 
+
+}
---
+++ src/main/java/co/edu/uniandes/dse/parcialejemplo/repositories/HotelRepository.java
@@ -0,0 +1,15 @@
+package co.edu.uniandes.dse.parcialejemplo.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.uniandes.dse.parcialejemplo.entities.HotelEntity;
+
+@Repository
+public interface HotelRepository extends JpaRepository<HotelEntity, Long> {
+    Optional<HotelEntity> findById(Long id);
+    Optional<HotelEntity> findByNumIdentificacion(int numIdentificacion); 
+
+}
